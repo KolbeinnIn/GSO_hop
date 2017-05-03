959021ca574eb8ea7460560422a1c55e57ddf7f2
KolbeinnIn <kolbeinnin264@nemi.tskoli.is>
2017-05-03 14:13:34 +0000

    Færslur næstum búnar

--- lokaverkefni.sql
+++ lokaverkefni.sql
@@ -4,7 +4,8 @@ CREATE TABLE utgefandi
 	ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	nafn VARCHAR(55) NOT NULL,
 	land VARCHAR(55),
-	arStofnad CHAR(4)
+	arStofnad CHAR(4),
+	stofnandi VARCHAR(55)
 );
 CREATE TABLE flokkur(
 	ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -46,9 +47,9 @@ CREATE TABLE lag(
 INSERT INTO
 	utgefandi(nafn,land,arStofnad)
 VALUES
-	("Kristbergur Útgáfa", "Ísland","1978"),
-	("SH Records", "Bandaríkin", "2001"),
-	("Fueled by Ramen LLC.","Bandaríkin", "1996");
+	("Kristbergur Útgáfa", "Ísland","1978","Kristberg Rúnar Pálsson"),
+	("SH Records", "Bandaríkin", "2001","Sherlock Holmes"),
+	("Fueled by Ramen LLC.","Bandaríkin", "1996","Vinnie Fiorello");
 	
 INSERT INTO
 	flokkur(nafn)
@@ -62,13 +63,13 @@ VALUES
 INSERT INTO
 	tegund(nafn)
 VALUES
-	("Popp"),
-	("Rokk"),
-	("Jazz"),
-	("Blues"),
-	("Reggae"),
-	("Hip Hop"),
-	("Rapp");
+	1("Popp"),
+	2("Rokk"),
+	3("Electronic"),
+	4("Indie"),
+	5("Reggae"),
+	6("Hip Hop"),
+	7("Rapp");
 INSERT INTO
 	flytjandi(nafn,faedingardagur,danardagur,flokkur_ID)
 VALUES
@@ -76,18 +77,44 @@ VALUES
 	("Floof", 1998-03-29,2004-06-10,2),
 	("Kristberg ´Poof´ Pálsson & Maggi",2005-05-05,NULL,3),
 	("Karmin", 2012-01-10,NULL,4),
-	("Kvennakórinn Sóley",1976-10-22,1995-02-17,5);
+	("Kvennakórinn Sóley",1976-10-22,1995-02-17,5),
+	("TwentyØnePiløts",2009-12-01,NULL,3);
 	
 INSERT INTO
 	diskur(nafn,utgafudagur,utgefandi_ID)
 VALUES
-	("Pen",2017-05-03,1),
-	("Blurryface",2015-05-17,2),
-	("Rain",2016-09-26,3),
-	("Vessel",2013-01-08,3),
-	("Regional at Best",2011-07-08,2),
-	("Drought",2013-12-05,1),
-	("Gravity",2015-02-17,3);
+	1("Pen",2017-05-03,1),
+	2("Blurryface",2015-05-17,2),
+	3("Rain",2016-09-26,3),
+	4("Vessel",2013-01-08,3),
+	5("Regional at Best",2011-07-08,2),
+	6("Drought",2013-12-05,1),
+	7("Gravity",2015-02-17,3);
+
+INSERT INTO
+	lag(nafn,hofundur,lengd,texti,tegund_ID,flytjandi_ID,diskur_ID)
+VALUES
+   --("LAG",         00:00:00, "Texti", tegund,flytjandi,diskur)
+	("Guns for Hands",00:04:36,"TEXTI",1,6,4),
+	("Holding On to You",00:04:25,"TEXTI",4,6,5),
+	("Car Radio",00:04:27,"TEXTI",1,6,5),
+	("Forest",00:04:26,"TEXTI",1,6,5),
+	("Kitchen Sink",00:05:34,"TEXTI",1,6,5),
+	("Semi-Automatic",00:04:14,"TEXTI",1,6,4),
+	("Trees",00:04:27,"TEXTI",3,6,4),
+	("HeavyDirtySoul",00:03:55,"TEXTI",2,1,1),
+	("Ride",00:03:35,"TEXTI",2,2,3),
+	("Lane Boy",00:04,13,"TEXTI",2,3,7),
+	("Tear in My Heart",00:03:08,"TEXTI",)
+	("The Judge",00:04:58,"TEXTI",),
+	("Goner",00:03:57,"TEXTI",),
+	("Polarize",00:03:47,"TEXTI",),
+	("Message Man",00:04:00,"TEXTI",),
+	(""),
+	(""),
+	(""),
+	(""),
+	("");
 
 	
 	
